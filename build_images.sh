1c35a0f7608eb66ba8d40224b4b3412d26525968
Ulysse Rubens <ulyrubens@hotmail.com>
2018-11-21 13:34:42 +0100

    New helper scripts to set versions, build and push Docker images

---
+++ build_images.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+. ./versions.sh
+
+docker build -t $NAMESPACE/base:$BASE_VERSION base/
+docker build -t $NAMESPACE/java8:$JAVA8_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION java8/
+docker build -t $NAMESPACE/tomcat7:$TOMCAT7_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$JAVA8_VERSION tomcat7/
+
+docker build -t $NAMESPACE/backup:$BACKUP_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION auto_backup/
+docker build -t $NAMESPACE/mongodb:$MONGODB_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION mongodb/
+docker build -t $NAMESPACE/postgres:$POSTGRES_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION postgres/
+docker build -t $NAMESPACE/postgis:$POSTGIS_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$POSTGRES_VERSION postgis/
+
+docker build -t $NAMESPACE/memcached:$MEMCACHED_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION memcached/
+docker build -t $NAMESPACE/rabbitmq:$RABBITMQ_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION rabbitmq/
+docker build -t $NAMESPACE/nginx:$NGINX_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION nginx/
+
+docker build -t $NAMESPACE/iip-jp2000:$IIPJP2_VERSION iipJpeg2000/
+docker build -t $NAMESPACE/slurm:$SLURM_VERSION slurm/
+
+# Temporary
+docker build -t $NAMESPACE/retrieval:$RETRIEVAL_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$JAVA8_VERSION retrieval/
+docker build -t $NAMESPACE/bioformat:$BIOFORMAT_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$JAVA8_VERSION bioformat/
+docker build -t $NAMESPACE/iris:$IRIS_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$TOMCAT7_VERSION iris/
+#docker build -t $NAMESPACE/iip-cyto:$IIPCYTO_VERSION --build-arg FROM_NAMESPACE=$NAMESPACE --build-arg FROM_VERSION=$BASE_VERSION iipCyto/
---
+++ push_images.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+. ./versions.sh
+
+docker push $NAMESPACE/base:$BASE_VERSION
+docker push $NAMESPACE/java8:$JAVA8_VERSION
+docker push $NAMESPACE/tomcat7:$TOMCAT7_VERSION
+
+docker push $NAMESPACE/backup:$BACKUP_VERSION
+docker push $NAMESPACE/mongodb:$MONGODB_VERSION
+docker push $NAMESPACE/postgres:$POSTGRES_VERSION
+docker push $NAMESPACE/postgis:$POSTGIS_VERSION
+
+docker push $NAMESPACE/memcached:$MEMCACHED_VERSION
+docker push $NAMESPACE/rabbitmq:$RABBITMQ_VERSION
+docker push $NAMESPACE/nginx:$NGINX_VERSION
+
+docker push $NAMESPACE/iip-jp2000:$IIPJP2_VERSION
+docker push $NAMESPACE/slurm:$SLURM_VERSION
+
+# Temporary
+docker push $NAMESPACE/retrieval:$RETRIEVAL_VERSION
+docker push $NAMESPACE/bioformat:$BIOFORMAT_VERSION
+docker push $NAMESPACE/iris:$IRIS_VERSION
+#docker push $NAMESPACE/iip-cyto:$IIPCYTO_VERSION
+
