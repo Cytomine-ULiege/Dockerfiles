383be0fc1b8c7321e3ae81a7118efe5d6d2dcb79
Ulysse Rubens <ulyrubens@hotmail.com>
2019-06-11 16:15:22 +0200

    Migrate to PostgreSQL 11 + externalize config + extend official postgres Docker image

---
+++ postgres/init-db.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright (c) 2009-2019. Authors: see NOTICE file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER docker WITH SUPERUSER PASSWORD 'docker';
+    CREATE DATABASE docker;
+    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "docker" --dbname "docker" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS ltree;
+
+    GRANT ALL ON geometry_columns TO PUBLIC;
+	GRANT ALL ON geography_columns TO PUBLIC;
+	GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "docker" --dbname "docker" -f /tmp/kmeans-${KMEANS_VERSION}/kmeans.sql
